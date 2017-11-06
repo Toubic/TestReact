622d09d477536178685258158fee3db652e0a7c0
Toubic <tj22fe@student.lnu.se>
2017-11-06 01:54:28 +0100

    + componentWillMount in Todos.js

--- test/src/Components/Todos.js
+++ test/src/Components/Todos.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
 
 class Todos extends Component {
-  render() {
-    return (
-      <ul className="Todos">
-        <li>Todo 1</li>
-        <li>Todo 2</li>
-        <li>Todo 3</li>
-      </ul>
-    );
-  }
+
+
+    constructor(){
+        super();
+        this.state = {
+            todos: []
+        }
+    }
+
+    componentWillMount(){
+        this.setState({
+            todos: [
+                {
+                    todo: "Todo 1",
+                    type: "Type 1"
+                },
+                {
+                    todo: "Todo 2",
+                    type: "Type 2"
+                },
+                {
+                    todo: "Todo 3",
+                    type: "Type 3"
+                }
+            ]
+        }
+        )
+    }
+
+    render() {
+        return (
+            <div className="Todos">
+                <h2>Todos:</h2>
+                <ul>
+                    <li>{this.state.todos[0].todo} ({this.state.todos[0].type})</li>
+                    <li>{this.state.todos[1].todo} ({this.state.todos[1].type})</li>
+                    <li>{this.state.todos[2].todo} ({this.state.todos[2].type})</li>
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default Todos;
